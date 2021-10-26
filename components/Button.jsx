1837ce5c329782d5589ba137cbfc31b9e0b62c12
Archie Baer <archie@abaer.dev>
2021-10-26 14:22:22 +0100

    Button inline styles

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,47 +1,51 @@
 import React from "react";
-import { StyleSheet, Text, Pressable } from "react-native";
+import { Text, Pressable } from "react-native";
 
 export const PositiveButton = ({ children, ...props }) => {
   return (
-    <Pressable {...props} style={PositiveButtonStyles.button}>
-      <Text style={PositiveButtonStyles.text}>{children}</Text>
+    <Pressable
+      {...props}
+      style={{
+        backgroundColor: "#10b981",
+        width: 200,
+        height: 50,
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          color: "#ffffff",
+          fontSize: 20,
+        }}
+      >
+        {children}
+      </Text>
     </Pressable>
   );
 };
-
-const PositiveButtonStyles = StyleSheet.create({
-  button: {
-    backgroundColor: "#10b981",
-    width: 200,
-    height: 50,
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#ffffff",
-    fontSize: 20,
-  },
-});
 
 export const NegativeButton = ({ children, ...props }) => {
   return (
-    <Pressable {...props} style={NegativeButtonStyles.button}>
-      <Text style={NegativeButtonStyles.text}>{children}</Text>
+    <Pressable
+      {...props}
+      style={{
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          color: "#888888",
+          fontSize: 12,
+          fontWeight: "600",
+          paddingTop: 10,
+        }}
+      >
+        {children}
+      </Text>
     </Pressable>
   );
 };
-
-const NegativeButtonStyles = StyleSheet.create({
-  button: {
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#888888",
-    fontSize: 12,
-    fontWeight: "600",
-    paddingTop: 10,
-  },
-});
