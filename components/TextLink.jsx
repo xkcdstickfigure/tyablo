6d1f18473b031f02e302104fb3bf281dd875d89c
Archie Baer <archie@abaer.dev>
2021-10-26 23:11:01 +0100

    Opens tyablo web links using base url in config

--- components/TextLink.jsx
+++ components/TextLink.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Linking, Text } from "react-native";
 import colors from "../colors";
+import { SERVER } from "../config";
 
 export const TextLink = ({ href, to, ...props }) => {
   return (
     <Text
-      onPress={() =>
-        Linking.openURL(to ? `https://ty.altrazio.com/go/${to}` : href)
-      }
+      onPress={() => Linking.openURL(to ? `${SERVER}/go/${to}` : href)}
       style={{
         color: colors.emerald[500],
       }}
