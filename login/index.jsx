ce4d60a7f157247b3eef63484500426395281ff6
Archie Baer <archie@abaer.dev>
2021-10-26 13:44:14 +0100

    Formatting

--- login/index.jsx
+++ login/index.jsx
@@ -3,33 +3,34 @@ import { StyleSheet, View, Text, Linking } from "react-native";
 import { PositiveButton, NegativeButton } from "../components/Button";
 
 export const Login = () => {
-    const [offset, setOffset] = useState(0);
-    const slide = () => {
-        let o = 0;
-        let ov = 0;
-        const i = setInterval(() => {
-            setOffset(o);
-            ov += 0.5;
-            o += ov;
-            console.log(o);
-            if (o > 100) {
-                setOffset(100);
-                clearInterval(i);
-            }
-        }, 10);
-    };
-    
+  const [offset, setOffset] = useState(0);
+  const slide = () => {
+    let o = 0;
+    let ov = 0;
+    const i = setInterval(() => {
+      setOffset(o);
+      ov += 0.5;
+      o += ov;
+      console.log(o);
+      if (o > 100) {
+        setOffset(100);
+        clearInterval(i);
+      }
+    }, 10);
+  };
 
   return (
-    <View style={{
+    <View
+      style={{
         flex: 1,
         display: "flex",
         flexDirection: "row",
         width: "200%",
-        marginLeft: `-${offset}%`
-    }}>
-        <Screen action={() => slide()} />
-        <Screen action={() => slide()} />
+        marginLeft: `-${offset}%`,
+      }}
+    >
+      <Screen action={() => slide()} />
+      <Screen action={() => slide()} />
     </View>
   );
 };
@@ -43,7 +44,11 @@ export const Screen = ({ action }) => {
       </View>
       <View>
         <PositiveButton onPress={action}>Get Started</PositiveButton>
-        <NegativeButton onPress={() => Linking.openURL("https://ty.altrazio.com/go/privacy")}>See Privacy Policy</NegativeButton>
+        <NegativeButton
+          onPress={() => Linking.openURL("https://ty.altrazio.com/go/privacy")}
+        >
+          See Privacy Policy
+        </NegativeButton>
       </View>
     </View>
   );
@@ -51,23 +56,23 @@ export const Screen = ({ action }) => {
 
 const styles = StyleSheet.create({
   container: {
-      width: "100%",
+    width: "100%",
     flex: 1,
     backgroundColor: "#ffffff",
     alignItems: "center",
     justifyContent: "space-between",
     paddingTop: 100,
-    paddingBottom: 50
+    paddingBottom: 50,
   },
   logo: {
     backgroundColor: "#eeeeee",
     width: 125,
     height: 125,
-    borderRadius: 30
+    borderRadius: 30,
   },
   title: {
     fontSize: 40,
     paddingTop: 10,
-    letterSpacing: 2.5
+    letterSpacing: 2.5,
   },
 });
