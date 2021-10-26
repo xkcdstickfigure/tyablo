f4a52fc374cd621be09e09e705c595f3e257ad00
Archie Baer <archie@abaer.dev>
2021-10-26 13:58:29 +0100

    Screen navigation

--- login/index.jsx
+++ login/index.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Linking } from "react-native";
+import { StyleSheet, View, Text, Linking, Dimensions } from "react-native";
 import { PositiveButton, NegativeButton } from "../components/Button";
 
+const { width } = Dimensions.get("screen");
+console.log(width);
+
 export const Login = () => {
+  const [oldScreen, setOldScreen] = useState("login");
+  const [currentScreen, setCurrentScreen] = useState("welcome");
+  const [sliding, setSliding] = useState(false);
+
   const [offset, setOffset] = useState(0);
-  const slide = () => {
+  const setScreen = (name) => {
+    setOldScreen(currentScreen);
+    setCurrentScreen(name);
+    setSliding(true);
     let o = 0;
     let ov = 0;
     const i = setInterval(() => {
       setOffset(o);
       ov += 0.5;
       o += ov;
-      console.log(o);
       if (o > 100) {
-        setOffset(100);
+        setOffset(0);
+        setSliding(false);
         clearInterval(i);
       }
     }, 10);
+  };
+
+  const screens = {
+    welcome: <Screen title="one" action={() => setScreen("login")} />,
+    login: <Screen title="two" action={() => setScreen("welcome")} />,
   };
 
   return (
@@ -25,22 +40,40 @@ export const Login = () => {
         flex: 1,
         display: "flex",
         flexDirection: "row",
-        width: "200%",
+        width: width * 2,
         marginLeft: `-${offset}%`,
       }}
     >
-      <Screen action={() => slide()} />
-      <Screen action={() => slide()} />
+      {Object.keys(screens).map((id) =>
+        id === currentScreen || id === oldScreen ? (
+          <View
+            style={{
+              position: "absolute",
+              top: 0,
+              left:
+                id === currentScreen
+                  ? sliding
+                    ? width
+                    : 0
+                  : sliding
+                  ? 0
+                  : width,
+            }}
+          >
+            {screens[id]}
+          </View>
+        ) : null
+      )}
     </View>
   );
 };
 
-export const Screen = ({ action }) => {
+export const Screen = ({ title, action }) => {
   return (
     <View style={styles.container}>
       <View>
         <View style={styles.logo} />
-        <Text style={styles.title}>tyablo</Text>
+        <Text style={styles.title}>{title}</Text>
       </View>
       <View>
         <PositiveButton onPress={action}>Get Started</PositiveButton>
@@ -56,9 +89,7 @@ export const Screen = ({ action }) => {
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    flex: 1,
-    backgroundColor: "#ffffff",
+    width,
     alignItems: "center",
     justifyContent: "space-between",
     paddingTop: 100,
