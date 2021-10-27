242c1ff33b7508cd95ca52ed4c18d88891ecbc25
Archie Baer <archie@abaer.dev>
2021-10-27 12:25:04 +0100

    app pages + improved context loading

--- App.jsx
+++ App.jsx
@@ -6,32 +6,50 @@ import { fetchContext } from "./util/fetchContext";
 import { Login } from "./login";
 import { Text } from "react-native";
 
+const pages = {
+  home: () => <Text>this is the homepage</Text>,
+};
+
 export const App = () => {
   const [token, setToken] = useState();
   const [newUser, setNewUser] = useState();
   const [context, setContext] = useState();
   const [signedIn, setSignedIn] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState("home");
+  const [pageParam, setPageParam] = useState();
+
+  // Page Navigation
+  const setPage2 = (id, param) => {
+    setPage(id);
+    setPageParam(param);
+  };
 
   // Load Token
   useEffect(async () => {
     const storedToken = await SecureStore.getItemAsync("token");
     if (storedToken) {
-      const ctx = await fetchContext(storedToken);
-      if (ctx) {
-        setToken(storedToken);
-        setContext(ctx);
-        setSignedIn(true);
-      }
+      setToken(storedToken);
+      setSignedIn(true);
     }
     setLoading(false);
   }, []);
 
   // Set Token
   useEffect(() => {
-    if (token !== undefined)
-      SecureStore.setItemAsync("token", token).catch(() => {});
+    SecureStore.setItemAsync("token", token).catch(() => {});
   }, [token]);
+
+  // Update Context
+  useEffect(async () => {
+    if (token && signedIn) {
+      const ctx = await fetchContext(token);
+      if (ctx) setContext(ctx);
+      else setSignedIn(false);
+    }
+  }, [token, page, signedIn]);
+
+  const Page = pages[page];
 
   return (
     <AppContext.Provider
@@ -44,10 +62,12 @@ export const App = () => {
         context,
         setSignedIn,
         signedIn,
+        setPage: setPage2,
+        page,
+        pageParam,
       }}
     >
-      {!loading &&
-        (signedIn ? <Text>{context.user.name} is signed in</Text> : <Login />)}
+      {!loading && (signedIn ? context && <Page /> : <Login />)}
     </AppContext.Provider>
   );
 };
