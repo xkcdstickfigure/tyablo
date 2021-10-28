00b4fd9031dac24c4f6f5d1eca91242f672dd521
Archie Baer <archie@abaer.dev>
2021-10-28 04:34:04 +0100

    Post input max length

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -211,6 +211,7 @@ const PostEditor = ({ close }) => {
       <Input
         autoFocus
         multiline
+        maxLength={2000}
         style={{
           width: "100%",
           height: 80,
