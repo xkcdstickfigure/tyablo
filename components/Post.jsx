4d52357ce53faeaa2f5e7bd0596fe54b3c667e05
Archie Baer <archie@abaer.dev>
2021-10-28 02:51:49 +0100

    Like button

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,53 +1,84 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, Pressable } from "react-native";
 import colors from "../colors";
+import { Heart } from "react-native-feather";
 
-export const Post = ({ data }) => (
-  <View
-    style={{
-      backgroundColor: "#ffffff",
-      borderWidth: 1,
-      borderColor: colors.gray[200],
-      borderRadius: 10,
-      padding: 10,
-      paddingBottom: 15,
-    }}
-  >
+export const Post = ({ data }) => {
+  const [liked, setLiked] = useState(false);
+
+  return (
     <View
       style={{
-        flexDirection: "row",
-        alignItems: "center",
-        flexGrow: 1,
-        flexShrink: 1,
+        backgroundColor: "#ffffff",
+        borderWidth: 1,
+        borderColor: colors.gray[200],
+        borderRadius: 10,
+        padding: 10,
+        paddingBottom: 15,
       }}
     >
-      <Image
-        source={{ uri: data.author.avatar }}
+      <View
         style={{
-          width: 35,
-          height: 35,
-          borderWidth: 1,
-          borderColor: colors.gray[200],
-          borderRadius: 10,
-          marginRight: 10,
-        }}
-      />
-      <Text
-        numberOfLines={1}
-        style={{
-          fontSize: 16,
-          flexShrink: 1,
+          flexDirection: "row",
+          alignItems: "center",
         }}
       >
-        {data.author.name}
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            flexGrow: 1,
+          }}
+        >
+          <Image
+            source={{ uri: data.author.avatar }}
+            style={{
+              width: 35,
+              height: 35,
+              borderWidth: 1,
+              borderColor: colors.gray[200],
+              borderRadius: 10,
+              marginRight: 10,
+            }}
+          />
+          <Text
+            numberOfLines={1}
+            style={{
+              fontSize: 16,
+              flexShrink: 1,
+            }}
+          >
+            {data.author.name}
+          </Text>
+        </View>
+        <View
+          style={{
+            marginRight: 5,
+          }}
+        >
+          <Pressable
+            hitSlop={10}
+            onPress={() => {
+              setLiked(!liked);
+            }}
+          >
+            <Heart
+              width={20}
+              height={20}
+              stroke={colors.red[500]}
+              strokeWidth={1}
+              fill={liked ? colors.red[500] : null}
+            />
+          </Pressable>
+        </View>
+      </View>
+      <Text
+        style={{
+          marginTop: 10,
+        }}
+      >
+        {data.content}
       </Text>
     </View>
-    <Text
-      style={{
-        marginTop: 10,
-      }}
-    >
-      {data.content}
-    </Text>
-  </View>
-);
+  );
+};
